5b113dc7c390ba104caf8e7d4b0b9d7ef9ff8a70
Joyce <hellojoycewong@gmail.com>
2020-09-21 11:13:22 -0700

    finished makeBoard function

--- connect4.js
+++ connect4.js
@@ -8,8 +8,8 @@
 var WIDTH = 7;
 var HEIGHT = 6;
 
-var currPlayer = 1; // active player: 1 or 2
-var board = []; // array of rows, each row is array of cells  (board[y][x])
+let currPlayer = 1; // active player: 1 or 2
+let board = []; // array of rows, each row is array of cells  (board[y][x])
 
 /** makeBoard: create in-JS board structure:
  *    board = array of rows, each row is array of cells  (board[y][x])
@@ -17,6 +17,13 @@ var board = []; // array of rows, each row is array of cells  (board[y][x])
 
 function makeBoard() {
   // TODO: set "board" to empty HEIGHT x WIDTH matrix array
+  for (let i = 0; i < HEIGHT; i++){
+    board.push([])
+    for (let j = 0; j < WIDTH; j++){
+      board[i].push([null])
+    }
+  }
+  console.log(board)
 }
 
 /** makeHtmlBoard: make HTML table and row of column tops. */
